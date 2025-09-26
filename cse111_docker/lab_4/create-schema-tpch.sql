ef2098ee705053737315188739fbad7d3b0a7997
Gavin Abrigo <135771042+gav-ip@users.noreply.github.com>
2025-09-26 13:53:42 -0700

    lab 4

--- cse111_docker/lab_4/create-schema-tpch.sql
+++ cse111_docker/lab_4/create-schema-tpch.sql
@@ -223,3 +223,170 @@ JOIN nation n ON c.c_nationkey = n.n_nationkey
 JOIN region r ON n.n_regionkey = r.r_regionkey
 WHERE r.r_name = 'AMERICA'
     AND c.c_mktsegment = 'FURNITURE';
+
+SELECT 
+    n.n_name AS country,
+    COUNT(*) AS orders_count
+FROM orders o
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation n ON c.c_nationkey = n.n_nationkey
+JOIN region r ON n.n_regionkey = r.r_regionkey
+WHERE r.r_name = 'EUROPE'
+GROUP BY n.n_nationkey, n.n_name
+ORDER BY orders_count DESC;
+
+SELECT COUNT(*) AS total_orders_EGYPT
+FROM orders O
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation n ON c.c_nationkey = n.n_nationkey
+WHERE n.n_name = 'EGYPT' 
+  AND o.o_orderdate LIKE '1992%';
+
+SELECT 
+  c.c_name AS customer_name,
+  COUNT(*) AS num_orders,
+  SUM(o.o_totalprice) AS total_price
+FROM orders o
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation n ON c.c_nationkey = n.n_nationkey
+WHERE n.n_name = 'ARGENTINA'
+  AND o.o_orderdate LIKE '1996%'
+GROUP BY c.c_custkey, c.c_name
+ORDER BY total_price DESC;
+
+
+SELECT 
+    s.s_name AS supplier_name,
+    COUNT(*) AS box_parts_count
+FROM part p
+JOIN partsupp ps ON p.p_partkey = ps.ps_partkey
+JOIN supplier s ON ps.ps_suppkey = s.s_suppkey
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+WHERE n.n_name = 'KENYA'
+    AND p.p_container LIKE '%BOX%'
+GROUP BY s.s_suppkey, s.s_name
+ORDER BY box_parts_count DESC;
+
+SELECT 
+  n.n_name AS nation_name,
+  s.s_suppkey AS supplier_id
+FROM supplier s
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+WHERE n.n_name IN ('ARGENTINA', 'BRAZIL');
+
+SELECT 
+    s.s_name AS supplier_name,
+    o.o_orderpriority AS order_priority,
+    COUNT(DISTINCT p.p_partkey) AS unique_parts_count
+FROM supplier s
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+JOIN partsupp ps ON s.s_suppkey = ps.ps_suppkey
+JOIN part p ON ps.ps_partkey = p.p_partkey
+JOIN lineitem l ON p.p_partkey = l.l_partkey AND s.s_suppkey = l.l_suppkey
+JOIN orders o ON l.l_orderkey = o.o_orderkey
+WHERE n.n_name = 'INDONESIA'
+GROUP BY s.s_suppkey, s.s_name, o.o_orderpriority
+ORDER BY s.s_name, o.o_orderpriority;
+
+SELECT 
+    n.n_name AS nation_name,
+    o.o_orderstatus AS order_status,
+    COUNT(*) AS order_count
+FROM orders o
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation n ON c.c_nationkey = n.n_nationkey
+JOIN region r ON n.n_regionkey = r.r_regionkey
+WHERE r.r_name = 'AFRICA'
+GROUP BY n.n_nationkey, n.n_name, o.o_orderstatus
+ORDER BY n.n_name, o.o_orderstatus;
+
+SELECT COUNT(DISTINCT o.o_clerk) AS different_clerks_count
+FROM supplier s
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+JOIN lineitem l ON s.s_suppkey = l.l_suppkey
+JOIN orders o ON l.l_orderkey = o.o_orderkey
+WHERE n.n_name = 'PERU';
+
+SELECT 
+    n.n_name AS nation_name,
+    COUNT(DISTINCT o.o_orderkey) AS completed_orders_1993
+FROM supplier s
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+JOIN region r ON n.n_regionkey = r.r_regionkey
+JOIN lineitem l ON s.s_suppkey = l.l_suppkey
+JOIN orders o ON l.l_orderkey = o.o_orderkey
+WHERE r.r_name = 'AFRICA'
+    AND o.o_orderstatus = 'F'
+    AND o.o_orderdate LIKE '1993%'
+GROUP BY n.n_nationkey, n.n_name
+HAVING COUNT(DISTINCT o.o_orderkey) > 200
+ORDER BY completed_orders_1993 DESC;
+
+SELECT 
+    p.p_partkey AS part_key,
+    p.p_name AS part_name,
+    p.p_type AS part_type,
+    MIN(l.l_discount) AS min_discount,
+    MAX(l.l_discount) AS max_discount
+FROM part p
+JOIN lineitem l ON p.p_partkey = l.l_partkey
+WHERE p.p_type LIKE '%ECONOMY%' OR p.p_type LIKE '%COPPER%'
+GROUP BY p.p_partkey, p.p_name, p.p_type
+ORDER BY p.p_partkey;
+
+SELECT COUNT(DISTINCT o.o_orderkey) AS distinct_orders_negative_customer_positive_supplier
+FROM orders o
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN lineitem l ON o.o_orderkey = l.l_orderkey
+JOIN supplier s ON l.l_suppkey = s.s_suppkey
+WHERE c.c_acctbal < 0 
+    AND s.s_acctbal > 0;
+
+SELECT 
+    r.r_name AS region_name,
+    MAX(s.s_acctbal) AS max_account_balance
+FROM supplier s
+JOIN nation n ON s.s_nationkey = n.n_nationkey
+JOIN region r ON n.n_regionkey = r.r_regionkey
+GROUP BY r.r_regionkey, r.r_name
+HAVING MAX(s.s_acctbal) > 9000
+ORDER BY max_account_balance DESC;
+
+SELECT MIN(o.o_totalprice) AS min_order_total_price_cross_region
+FROM orders o
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation cn ON c.c_nationkey = cn.n_nationkey
+JOIN region cr ON cn.n_regionkey = cr.r_regionkey
+JOIN lineitem l ON o.o_orderkey = l.l_orderkey
+JOIN supplier s ON l.l_suppkey = s.s_suppkey
+JOIN nation sn ON s.s_nationkey = sn.n_nationkey
+JOIN region sr ON sn.n_regionkey = sr.r_regionkey
+WHERE cr.r_regionkey != sr.r_regionkey;
+
+SELECT COUNT(*) AS line_items_asia_suppliers_kenya_customers
+FROM lineitem l
+JOIN supplier s ON l.l_suppkey = s.s_suppkey
+JOIN nation sn ON s.s_nationkey = sn.n_nationkey
+JOIN region sr ON sn.n_regionkey = sr.r_regionkey
+JOIN orders o ON l.l_orderkey = o.o_orderkey
+JOIN customer c ON o.o_custkey = c.c_custkey
+JOIN nation cn ON c.c_nationkey = cn.n_nationkey
+WHERE sr.r_name = 'ASIA'
+    AND cn.n_name = 'KENYA';
+
+WITH ranked_suppliers AS (
+    SELECT 
+        r.r_name AS region_name,
+        s.s_name AS supplier_name,
+        s.s_acctbal AS account_balance,
+        ROW_NUMBER() OVER (PARTITION BY r.r_regionkey ORDER BY s.s_acctbal DESC) as rn
+    FROM supplier s
+    JOIN nation n ON s.s_nationkey = n.n_nationkey
+    JOIN region r ON n.n_regionkey = r.r_regionkey
+)
+SELECT region_name, supplier_name, account_balance
+FROM ranked_suppliers
+WHERE rn = 1
+ORDER BY region_name;
+
+
